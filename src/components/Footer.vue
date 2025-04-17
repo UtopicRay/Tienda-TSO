8dff8bdfb1c0353f99d8da6599de2d44ef9a49f2
UtopicRay <rayrm@gmail.com>
2025-04-17 06:17:47 -0400

    'Completado Footer y SuscribeSection.vue'

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,40 +2,46 @@
 
 import { defineComponent } from 'vue'
 import SimpleForm from '@/components/SimpleForm.vue'
+import IconFacebook from '@/components/icons/IconFacebook.vue'
+import IconInstagram from '@/components/icons/IconInstagram.vue'
+import IconPeoples from '@/components/icons/IconPeoples.vue'
+import IconEspañaBrand from '@/components/icons/IconEspañaBrand.vue'
 
 export default defineComponent({
-  components: { SimpleForm }
+  components: { IconEspañaBrand, IconPeoples, IconInstagram, IconFacebook, SimpleForm }
 })
 </script>
 <template>
   <footer>
     <div class="footer-container">
-      <div class="mx-auto flex justify-between items-center w-full">
-        <div>
+      <div class="grid grid-cols-4 items-center   w-full">
+        <div class="mx-auto">
           <p class="text-grey-color">¿Cómo podemos ayudarte?</p>
           <h4 class="text-white text-2xl">Ponte en contacto con
             nosotros en cualquier
             momento</h4>
         </div>
-        <div class="flex flex-col gap-2">
+        <div class="flex flex-col gap-2 mx-auto">
           <p class="text-white">WhatsApp</p>
           <p class="text-grey-color">+(34) 624 60 67 70</p>
           <p class="text-white">Envíanos un mensaje</p>
           <p class="text-grey-color">contacto@tsotienda.com</p>
         </div>
-        <div>
+        <div class="mx-auto">
           <label class="text-white">Suscríbase a nuestro boletín</label>
           <SimpleForm class="bg-white border border-red-500" placeholder="Correo" action="Suscribirse" color="bg-red-500" type="email"/>
         </div>
-        <div>
-          <p>Siguenos</p>
+        <div class="mx-auto">
+          <p class="text-grey-color">Siguenos</p>
           <ul class="flex gap-3">
-            <li class="text-white">facebook-icon</li>
-            <li class="text-white">facebook-icon</li>
-            <li class="text-white">facebook-icon</li>
+            <li class="text-white"><IconFacebook/></li>
+            <li class="text-white"><IconPeoples/></li>
+            <li class="text-white"><IconInstagram/></li>
           </ul>
         </div>
       </div>
+      <!--Divider-->
+      <div class="border border-[#1A232F]"></div>
       <div class="flex justify-between items-center">
         <div class="flex flex-col gap-4">
           <p class="font-bold">Enlaces de interés</p>
@@ -67,6 +73,8 @@ export default defineComponent({
     </div>
     <div class="copyright">
       <p class="text-grey-color">Copyright © TSO 2023 - Madrid</p>
+      <p>Español</p>
+      <IconEspañaBrand/>
     </div>
   </footer>
 </template>
