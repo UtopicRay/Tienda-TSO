e4ffec5c1111ac9cf05924773803cf2c1d900085
UtopicRay <rayrm@gmail.com>
2025-04-18 17:59:58 -0400

    Add images categorys

---
+++ src/components/sections/UltimosSection.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import Title from '@/components/Title.vue'
+</script>
+
+<template>
+  <section class="section bg-light-grey-2">
+    <Title title="Últimos añadidos" />
+  </section>
+</template>
+
+<style scoped>
+
+</style>
