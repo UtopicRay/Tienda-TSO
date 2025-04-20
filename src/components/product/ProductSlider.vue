8a78870f524828f4f6b941b56a8eec662966b8af
UtopicRay <rayrm@gmail.com>
2025-04-19 21:10:03 -0400

    'Completado ProductSlider.vue'

--- src/components/product/ProductSlider.vue
+++ src/components/product/ProductSlider.vue
@@ -1,21 +1,21 @@
 <script setup lang="ts">
 import ProductCard from '@/components/product/ProductCard.vue'
-import { useFechtData } from '@/hooks/useFechtData.ts'
-import { baseUrl } from '../../../env.ts'
 import Loading from '@/components/Loading.vue'
 import ErrorAlert from '@/components/ErrorAlert.vue'
-const { data: products, loading, error } = useFechtData(baseUrl)
+import { useProductStore } from '@/stores/product.ts'
+
+const {products,error,loading}=useProductStore()
 </script>
 
 <template>
-  <Loading v-if="loading" />
+  <Loading v-if="loading&&!products" />
   <div class="flex justify-center">
     <div class="relative w-full max-w-screen-xl overflow-hidden">
       <!-- Slider Wrapper -->
       <div class="flex flex-col lg:flex-row overflow-x-auto scroll-smooth gap-4 md:gap-6 lg:gap-8 px-4 py-6">
         <ProductCard
           :product="product"
-          v-for="product in products?.products.slice(0, 10)"
+          v-for="product in products?.slice(0, 10)"
           :key="product?.id"
           class="flex-shrink-0"
         />
@@ -44,4 +44,5 @@ const { data: products, loading, error } = useFechtData(baseUrl)
 ::-webkit-scrollbar-thumb:hover {
   background: #555;
 }
+
 </style>
